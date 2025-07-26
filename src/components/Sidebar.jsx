256b158e595e9307423590b7fe9f2845d5631cd7
Ahsan Masood <135040587+ahsan123-png@users.noreply.github.com>
2025-07-26 19:41:31 +0500

    add cursor

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,7 +9,7 @@ export default function Sidebar({ users, selectedUser, onSelectUser }) {
             <img
               src="https://randomuser.me/api/portraits/men/32.jpg"
               alt="Profile"
-              className="w-10 h-10 rounded-full"
+              className="w-10 h-10 rounded-full cursor-pointer"
             />
             <span className="absolute bottom-0 right-0 w-3 h-3 bg-green-500 rounded-full border-2 border-white"></span>
           </div>
